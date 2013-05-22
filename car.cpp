babe75205d4ea3de2723bcfebe6055d802c95584
Sergiy Vashulenko <ginex62@gmail.com>
2013-05-22 10:33:24 +0300

    no errors

--- car.cpp
+++ car.cpp
@@ -1,17 +1,19 @@
-#include <iostream>
-#include <string> 
-#include <conio.h>
 #include "stdafx.h"
+#include <iostream>
+#include <string>
+#include <conio.h>
+
 using namespace std;
 
 class Car
 {
 public:
 //---------------------Type------------------------
-void setTypeCar( string Type )
+string setTypeCar( string Type )
 { 
 TypeCar = Type; 
-} 
+}
+//int 
 string getTypeCar() 
 { 
 return TypeCar; 
@@ -36,15 +38,15 @@ return WayCar;
 }
 void displayMessage()
 {
-cout «"Way:" « getWayCar() « endl;
-cout«"Type:" « getTypeCar() « endl;
-cout«"Price:" « getPriceCar() « endl;
+cout<<"Way:"<<getWayCar() <<endl;
+cout<<"Type:"<<getTypeCar()<<endl;
+cout<<"Price:"<<getPriceCar()<<endl;
 }
 private:
 string TypeCar; 
 int PriceCar;
 int WayCar;
-}; 
+//int; 
 
 int main()
 {
@@ -54,24 +56,25 @@ int WayCar;
 Car myCar; 
 
 
-cout « myCar.getTypeCar() « endl;
+cout<< myCar.getTypeCar()<<endl;
 
 
-cout « "\nPlease enter the car type:" « endl;
-cin»TypeCar; 
+cout<<"\nPlease enter the car type:"<<endl;
+cin>>TypeCar; 
 myCar.setTypeCar( TypeCar );
 
-cout « "\nPlease enter the car price:" « endl;
-cin»PriceCar; 
+cout<<"\nPlease enter the car price:"<<endl;
+cin>>PriceCar; 
 myCar.setPriceCar( PriceCar );
 
-cout « "\nPlease enter the car way:" « endl;
-cin»WayCar; 
+cout<<"\nPlease enter the car way:"<<endl;
+cin>>WayCar; 
 myCar.setWayCar( WayCar );
 
 myCar.displayMessage(); 
 
-cout « endl;
+cout<<endl;
 
 getch();
+}
 }
